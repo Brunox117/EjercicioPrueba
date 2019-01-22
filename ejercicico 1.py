f6af5db6da271cf071eb146008a6a5af3f307bb2
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2019-01-22 11:05:16 -0600

    Bruno Omar Jiménez Mancilla, Grupo 03

---
+++ ejercicico 1.py
@@ -0,0 +1,8 @@
+#Autor: Bruno Omar Jiménez Mancilla
+#Calcula velocidad promedio, dado e tiempo y distancia
+
+dist = int(input("Teclea la distancia en kms enteros: "))
+temp = int(input("Teclea el tiempo de tu viaje en hrs. enteras: "))
+
+velocidad = dist / temp
+print("Velocidad promedio: ", velocidad, "km/h")
